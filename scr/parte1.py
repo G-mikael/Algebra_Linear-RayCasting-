c5efff89556c933a4c92bd206c2991c28c3403bf
Gerardo Mikael Do Carmo Pereira <gerardomikael1234@gmail.com>
2022-10-16 23:15:40 -0300

    adicionar floorcasting e textura do céu

--- scr/parte1.py
+++ scr/parte1.py
@@ -1,29 +1,76 @@
 import pygame as pg
 from sys import exit
 import numpy as np
+import os
+import ntpath
 
-pg.init()
-screen = pg.display.set_mode((1280, 720))
-pg.display.set_caption('Yuri me da 10 pfv')
-#limitando o fps 
-clock = pg.time.Clock()
-running = True
+if __name__ == "__main__":
+    head, tail = ntpath.split(os.path.realpath(__file__))
+    os.chdir(head)
 
-superficie = pg.Surface((100, 200))
-superficie.fill('lightblue')
+def game():
+    pg.init()
+    screen = pg.display.set_mode((1280, 720))
+    pg.display.set_caption('Yuri me da 10 pfv')
+    #limitando o fps 
+    clock = pg.time.Clock()
+    running = True
+    res_horizontal = 216
+    half_res_vertical = 180
 
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+    #ângulo de visão do jogador
+    fov = res_horizontal/60
+    #posiçao inicial do jogador
+    posx, posy, rot = 0, 0, 0
+    frame = np.random.uniform(0,1, (res_horizontal, half_res_vertical*2, 3))
+
+    #texturas
+    ceu = pg.image.load('textures/sky.jpg')
+    ceu = pg.surfarray.array3d(pg.transform.scale(ceu, (360, half_res_vertical*2)))
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        
+        for i in range(res_horizontal):
+            rot_i = rot + np.deg2rad(i/fov - 30)
+            sin, cos, cos_correcao = np.sin(rot_i), np.cos(rot_i), np.cos(np.deg2rad(i/fov - 30))
+            frame[i][:] = ceu[int(np.rad2deg(rot_i)%360)][:]/255
+
+            for j in range(half_res_vertical):
+                n = (half_res_vertical/(half_res_vertical-j))/cos_correcao
+                x, y = posx + cos*n, posy + sin*n
+
+                if int(x)%2 == int(y)%2:
+                    frame[i][half_res_vertical*2-j-1] = [0, 0, 0]
+                else:
+                    frame[i][half_res_vertical*2-j-1] = [1, 1, 1]
+        
+        surf = pg.surfarray.make_surface(frame*255)
+        surf = pg.transform.scale(surf, (1280, 720))
+        screen.blit(surf, (0, 0))
+
+        pg.display.update()
+        
+        posx, posy, rot = movimentacao(posx, posy, rot, pg.key.get_pressed())
+        clock.tick(60)
+    pg.quit()
+    exit()
+
+def movimentacao(posx, posy, rot, keys):
+    if keys[ord('a')]:
+        rot = rot - 0.08
+
+    if keys[ord('d')]:
+        rot = rot + 0.08
     
-    #frame = np.random.uniform(0,1, (128, 72, 3))
-    #surf = pg.surfarray.make_surface(frame*255)
-    #surf = pg.transform.scale(surf, (1280, 720))
-    screen.blit(superficie, (100, 200))
+    if keys[ord('w')]:
+        posx, posy = posx + np.cos(rot)*0.12, posy + np.sin(rot)*0.12
 
-    pg.display.update()
-    clock.tick(60)
+    if keys[ord('s')]:
+        posx, posy = posx - np.cos(rot)*0.12, posy - np.sin(rot)*0.12
+    
+    return posx, posy, rot
 
-pg.quit()
-exit()
+game()
