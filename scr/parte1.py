fbd136d292316407974fef3782235236d9c83bd9
Gerardo Mikael Do Carmo Pereira <gerardomikael1234@gmail.com>
2022-10-18 00:15:21 -0300

    iniciar projeto do raycasting e adicionar texturas

--- scr/parte1.py
+++ scr/parte1.py
@@ -3,6 +3,7 @@ from sys import exit
 import numpy as np
 import os
 import ntpath
+from numba import njit
 
 if __name__ == "__main__":
     head, tail = ntpath.split(os.path.realpath(__file__))
@@ -15,38 +16,33 @@ def game():
     #limitando o fps usando o pg.time.Clock
     clock = pg.time.Clock()
     running = True
-    res_horizontal = 216
-    half_res_vertical = 180
+    res_horizontal = 480
+    half_res_vertical = 400
 
     #ângulo de visão do jogador
     fov = res_horizontal/60
     #posiçao inicial do jogador
-    posx, posy, rot = 0, 0, 0
+    posx, posy, rot = 3, 3, 0
     frame = np.random.uniform(0,1, (res_horizontal, half_res_vertical*2, 3))
 
+    #mapa
+    tamanho = 6
+    mapa = np.random.choice([0,0,0,0,0,1], (tamanho, tamanho))
+
     #texturas
-    ceu = pg.image.load('textures/sky.jpg')
+    ceu = pg.image.load('textures/sky.png')
     ceu = pg.surfarray.array3d(pg.transform.scale(ceu, (360, half_res_vertical*2)))
+    floor = pg.surfarray.array3d(pg.image.load('textures/floor2.png'))
+    wall = pg.surfarray.array3d(pg.image.load('textures/wall.png'))
+
 
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
         
-        for i in range(res_horizontal):
-            rot_i = rot + np.deg2rad(i/fov - 30)
-            sin, cos, cos_correcao = np.sin(rot_i), np.cos(rot_i), np.cos(np.deg2rad(i/fov - 30))
-            frame[i][:] = ceu[int(np.rad2deg(rot_i)%360)][:]/255
+        frame = numba_frame(floor, ceu, posx, posy, rot, frame, res_horizontal,  half_res_vertical, fov, mapa, tamanho, wall)
 
-            for j in range(half_res_vertical):
-                n = (half_res_vertical/(half_res_vertical-j))/cos_correcao
-                x, y = posx + cos*n, posy + sin*n
-
-                if int(x)%2 == int(y)%2:
-                    frame[i][half_res_vertical*2-j-1] = [0, 0, 0]
-                else:
-                    frame[i][half_res_vertical*2-j-1] = [1, 1, 1]
-        
         surf = pg.surfarray.make_surface(frame*255)
         surf = pg.transform.scale(surf, (1280, 720))
         screen.blit(surf, (0, 0))
@@ -76,5 +72,33 @@ def movimentacao(posx, posy, rot, keys):
         posx, posy = posx - np.cos(rot)*0.12, posy - np.sin(rot)*0.12
     
     return posx, posy, rot
+
+#otimizaçao do código usando numba (faz milagre essa porra)
+@njit()
+def numba_frame(floor, ceu, posx, posy, rot, frame, res_horizontal,  half_res_vertical, fov, mapa, tamanho, wall):
+    for i in range(res_horizontal):
+            rot_i = rot + np.deg2rad(i/fov - 30)
+            sin, cos, cos_correcao = np.sin(rot_i), np.cos(rot_i), np.cos(np.deg2rad(i/fov - 30))
+            frame[i][:] = ceu[int(np.rad2deg(rot_i)%360)][:]/255
+
+            for j in range(half_res_vertical):
+                n = (half_res_vertical/(half_res_vertical-j))/cos_correcao
+                x, y = posx + cos*n, posy + sin*n
+                xfloor, yfloor = int(x*2%1*300), int(y*2%1*300)
+            
+                if mapa[int(x)%(tamanho-1)][int(y)%(tamanho-1)]:
+                    h = half_res_vertical - j
+                    if x%1<0.02 or x%1>0.98:
+                        xfloor = yfloor
+                    yfloor = np.linspace(0,198, h*2)%99
+
+                    for k in range(h*2):
+                        frame[i][half_res_vertical - h + k] = wall[xfloor][int(yfloor[k])]/255
+                    break
+                else:
+                    frame[i][half_res_vertical*2-j-1] = floor[xfloor][yfloor]/255
+
+
+    return frame
 
 game()
